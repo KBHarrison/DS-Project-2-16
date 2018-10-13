e2fb30c4c9f72f31f36fcd3811b2c5a715e2d722
Kyle <kylebharrison16@gmail.com>
2018-10-13 16:55:36 -0600

    Added Dictionary, Home, and Stack controllers. Also fixed exit link to point to byu.edu

---
+++ Group Data Structures Assignment/Controllers/DictionaryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Group_Data_Structures_Assignment.Controllers
+{
+    public class DictionaryController : Controller
+    {
+        // GET: Dictionary
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Group Data Structures Assignment/Controllers/StackController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Group_Data_Structures_Assignment.Controllers
+{
+    public class StackController : Controller
+    {
+        // GET: Stack
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
